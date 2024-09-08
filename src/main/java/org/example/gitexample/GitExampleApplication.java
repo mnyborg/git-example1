0862666aa4c3d547ba757abfae537ca619d82f79
Mads Nyborg <MANY@kea.dk>
2024-09-08 10:13:52 +0200

    Initial commit

---
+++ src/main/java/org/example/gitexample/GitExampleApplication.java
@@ -0,0 +1,13 @@
+package org.example.gitexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitExampleApplication.class, args);
+    }
+
+}
